c80e260ed6106bf03ef600355a28b08c72080cee
Cedric Finance <cedric.finance@finandric.com>
2022-12-02 06:39:47 +0100

    [Aoc 2022 JS] Refactor day1 part1

--- js/2022/day02/part1.js
+++ js/2022/day02/part1.js
@@ -8,15 +8,32 @@ mapping.set("B", "paper")
 mapping.set("Z", "scissor")
 mapping.set("C", "scissor")
 
+const choiceLoseAgainst = new Map()
+choiceLoseAgainst.set("rock", "paper")
+choiceLoseAgainst.set("paper", "scissor")
+choiceLoseAgainst.set("scissor", "rock")
+
 const choiceScore = new Map()
 choiceScore.set("rock", 1)
 choiceScore.set("paper", 2)
 choiceScore.set("scissor", 3)
 
-const choiceToWin = new Map()
-choiceToWin.set("rock", "paper")
-choiceToWin.set("paper", "scissor")
-choiceToWin.set("scissor", "rock")
+const outcomeScore = new Map()
+outcomeScore.set("LOSE", 0)
+outcomeScore.set("DRAW", 3)
+outcomeScore.set("WIN", 6)
+
+function playRockPaperScissor(opponent, me) {
+  if (opponent == me) {
+    return "DRAW"
+  }
+
+  if (choiceLoseAgainst.get(opponent) == me) {
+    return "WIN"
+  }
+
+  return "LOSE"
+}
 
 aoc.run(function(input) {
     const lines = input.lines()
@@ -27,12 +44,7 @@ aoc.run(function(input) {
       const [opponent, me] = line.split(" ").map(i => mapping.get(i))
 
       myScore += choiceScore.get(me)
-
-      if (opponent == me) {
-        myScore += 3
-      } else if (choiceToWin.get(opponent) == me) {
-        myScore += 6
-      }
+      myScore += outcomeScore.get(playRockPaperScissor(opponent, me))
     }
     
   return myScore
